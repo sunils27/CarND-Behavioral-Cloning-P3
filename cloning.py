fb1e60b9b58af5e47bd9b46f554c0d08bcd674de
sunilstango <sunil.suram@tangomc.com>
2017-05-28 19:00:58 -0700

    working version before submission

--- cloning.py
+++ cloning.py
@@ -4,19 +4,19 @@
 # In[1]:
 
 from keras.models import Sequential
-from keras.layers import Flatten, Layer, Dense, Conv2D
+from keras.layers import Flatten, Layer, Dense, Convolution2D, Activation, Dropout
+from keras.layers import Cropping2D, MaxPooling2D
 import csv
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import keras
 
+from keras.layers.core import Lambda
 
 # In[2]:
 
 center_images = []
-left_images = []
-right_images = []
 steering = []
 
 
@@ -27,45 +27,71 @@ with open('./data/driving_log.csv', 'r') as drivinglogfile:
     next(drivinglogfile, None)
     reader = csv.reader(drivinglogfile)
     for line in reader:
-        #print(line[0].split("/")[-1])
-        img = cv2.imread("./data/IMG/"+line[0].split("/")[-1])
-        center_images.append(img)
-        #left_images.append(cv2.imread("./data-mysim/IMG/"+line[1].split("/")[-1]))
-        #right_images.append(cv2.imread("./data-mysim/IMG/"+line[2].split("/")[-1]))
-        steering.append(line[3])
+	    #print(line[0].split("/")[-1])
+	    for i in range(3):
+		    img = cv2.imread("./data/IMG/"+line[i].split("/")[-1])
+		    center_images.append(img)
+		    if ( i==1):
+			    steering.append(float(line[3]) + 0.18)
+		    if ( i==2 ):
+			    steering.append(float(line[3]) - 0.18)
+		    if ( i==0):
+			    steering.append(line[3])
+	    #print(line[3])
 
 
-# In[4]:
 
 print(len(center_images))
-print(len(left_images))
-print(len(right_images))
 print(len(steering))
 
 
-# In[5]:
 
 #convert training data to numpy arrays
 X_train = np.array(center_images)
 y_train = np.array(steering)
 print(keras.__version__)
 print(X_train.shape)
+input_shape = X_train.shape[1:]
 
 
 # In[6]:
 
 #create the DL model
 model = Sequential()
-#model.add( Flatten(input_shape=(160,320,3)) )
-model.add( Conv2D(32,3,3, subsample=(4,4), border_mode='valid', input_shape=(160,320,3)  ))
+model.add( Lambda(lambda x: x/255.0 - 0.5, input_shape=input_shape) )
+#60,30 below worked for the most part
+model.add(Cropping2D(cropping=((70,30), (0,0))))
+model.add(Convolution2D(16, 3, 3, subsample=(2, 2), border_mode="same") )
+model.add( Activation('relu'))
+
+model.add(Convolution2D(32, 5, 5, subsample=(2, 2), border_mode="same"))
+model.add( Activation('relu'))
+
+model.add(Convolution2D(32, 5, 5, subsample=(2, 2), border_mode="same"))
+model.add( Activation('relu'))
+model.add(Convolution2D(32, 5, 5, subsample=(2, 2), border_mode="same"))
+model.add( Activation('relu'))
+
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(.25))
+model.add(Flatten())
+
+model.add(Dense(16))
+model.add( Activation('relu'))
+model.add(Dense(16))
+model.add( Activation('relu'))
+#model.add(Dense(20))
+#model.add( Activation('relu'))
+#model.add(Dropout(.25))
+
 model.add(Dense(1))
 
 #compile the model
 model.compile( loss='mse', optimizer='adam')
 #fit data
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
-
 model.save('model.h5')
+
 # In[ ]:
 
 
